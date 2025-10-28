5b2de5313c872ab7b1d06a1ccc3edb87e3818444
vandat-snk <datlamson2005@gmail.com>
2025-10-28 19:21:51 +0700

    Cập nhật phần Hero Section và config

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  title: "Naresh Khatri | Full-Stack Developer",
+  title: "VanDat.1th1 | Full-Stack Developer",
   description: {
     long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
@@ -23,8 +23,8 @@ const config = {
     "Spline",
     "Framer Motion",
   ],
-  author: "Naresh Khatri",
-  email: "naresh.khatri2345@gmail.com",
+  author: "VanDat.1th1",
+  email: "datlamson2005@gmail.com",
   site: "https://nareshkhatri.site",
 
   get ogImg() {
@@ -33,9 +33,9 @@ const config = {
   social: {
     twitter: "https://x.com/nothotchaddi",
     linkedin: "https://www.linkedin.com/in/naresh-khatri/",
-    instagram: "https://www.instagram.com/hotchaddi",
-    facebook: "https://www.facebook.com/HotChaddi/",
-    github: "https://github.com/Naresh-Khatri",
+    instagram: "https://www.instagram.com/vnadat.1th1",
+    facebook: "https://www.facebook.com/vawnddatj",
+    github: "https://github.com/vandat-snk",
   },
 };
 export { config };
